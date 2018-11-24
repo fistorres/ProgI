ef4a6cadc90b967f73f784b5d6bb4b2b2f43d213
Gil Oliveira <mariogil@edu.ulisboa.pt>
2018-11-24 16:56:51 +0000

    Adicionei uma função writeHeader, que escreve o cabeçalho dos ficheiros

--- filesWriting.py
+++ filesWriting.py
@@ -2,3 +2,16 @@
 # Grupo 38
 # 49187 Sofia Torres
 # 49269 Mário Gil Oliveira
+
+
+def writeHeader(fileName,day,time,type,company):
+    """
+    Opens a file in write mode and writes the required header in the first lines.
+    Requires: fileName is str of a .txt file, day is str in YYYY-MM-DD format,
+    time is str in HH:MM format, and company is str, type is str.
+    Ensures: the creation of a file with the required header as stated in the project
+    which states the day, time, time and type of file (e.g. schedule).
+    """
+    file = open(fileName, 'w')
+    File.writelines(['Day: \n', day, 'Time: \n', time, 'Company: \n', company, type': \n'])
+    file.close()
