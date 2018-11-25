21b1bfeeebe5cac0686abd4a84d00dd23a49a8fc
Gil Oliveira <mariogil@edu.ulisboa.pt>
2018-11-25 11:50:42 +0000

    Os módulos de escrita parecem estar a funcionar até agora, ainda que estejam incompletos

--- filesWriting.py
+++ filesWriting.py
@@ -4,14 +4,31 @@
 # 49269 Mário Gil Oliveira
 
 
-def writeHeader(fileName,day,time,type,company):
+def newFile(year, month, day, hour, minute, fileType, company):
     """
     Opens a file in write mode and writes the required header in the first lines.
-    Requires: fileName is str of a .txt file, day is str in YYYY-MM-DD format,
-    time is str in HH:MM format, and company is str, type is str.
-    Ensures: the creation of a file with the required header as stated in the project
-    which states the day, time, time and type of file (e.g. schedule).
+    Requires: y,mo,d,h, min are int, and company is str, fileType is str.
+    Ensures: the creation of a file with the required file name and header
+    as stated in the project, the file is left open.
     """
-    file = open(fileName, 'w')
-    File.writelines(['Day: \n', day, 'Time: \n', time, 'Company: \n', company, type': \n'])
+    fileName = str(year)+'y'+str(month)+'m'+str(day)+fileType+str(hour)+'h'+str(minute)+'.txt'
+    file = open(fileName, 'w')  # opens a new file in write mode w/ fileName
+    date = str(year)+'-'+str(month)+'-'+str(day)
+    time = str(hour)+':'+str(minute)
+    file.writelines(['Day: \n', date, '\n', 'Time: \n', time, '\n',
+                     'Company: \n', company, '\n', fileType.capitalize(), ': \n'])
+    file.close()
+
+
+def addSchedule(year, month, day, hour, minute, customer, worker):
+    """
+    Opens a schedule file in append mode, adds a new entry, then closes the file
+    Requires: year, month, day, hour, minute are int, customer, worker are str
+    Ensures: Adds a new line to the corresponding schedule file with the inputed
+    information
+    """
+    fileName = str(year)+'y'+str(month)+'m'+str(day)+'schedule'+str(hour)+'h'+str(minute)+'.txt'
+    file = open(fileName, 'a')  # opens the corresponding file in append mode
+    file.write(str(year) + '-' + str(month) + '-'+str(day) + ', ' +
+               str(hour) + ':' + str(minute)+', ' + customer + ', ' + worker)
     file.close()
