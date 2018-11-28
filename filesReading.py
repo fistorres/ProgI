744d6f53668bb52ef74c886e6d194d3c35cb0ab5
fistorres <45104156+fistorres@users.noreply.github.com>
2018-11-28 21:50:11 +0000

    Update filesReading.py

--- filesReading.py
+++ filesReading.py
@@ -26,12 +26,20 @@ def readExpertsFile(fileName):
     i = 0
     while i < lenfile-7:
         outputList.append(fileIn.readline().replace("\n", "").split(","))
-        i = i+i
+        i = i+1
 
     return outputList
 
 
-
+def tupexp(fileName):
+    """ Turn string of fields of experts into a tuple
+    Requires: fileName is str, the name of a .txt file listing experts,
+    Ensures : tuple of fields of all the experts in the list """
+    explist = readExpertsFile("2019y01m12experts09h00.txt")
+    for i in explist:
+        i[2] = tuple(i[2].replace(";",",").replace("(","").replace(")","").split(","))
+        
+        
 def readHeader(fileName):
     """
     Converts a given file listing experts and returns day, time,
