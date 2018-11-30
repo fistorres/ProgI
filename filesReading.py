502a8c74f13f3f0a185ba8293fc07340c27fa43b
fistorres <45104156+fistorres@users.noreply.github.com>
2018-11-30 00:19:05 +0000

    Update filesReading.py

--- filesReading.py
+++ filesReading.py
@@ -3,16 +3,20 @@
 # 49187 Sofia Torres
 # 49269 Mário Gil Oliveira
 
-import os
-#  os.chdir('C:\\Users\\Sofia Torres\\Desktop\\3A1S\\PROGI\\projeto\\tests\\example1')
+# 2018-2019 Programação 1 (LTI)
+# Grupo 38
+# 49187 Sofia Torres
+# 49269 Mário Gil Oliveira
+
+
 import datetime
 
-def readExpertsFile(fileName):
+def readFile(fileName):
     """
-    Converts a given file listing experts into a collection
+    Converts a given file listing experts or clients into a collection
     Requires: fileName is str, the name of a .txt file listing experts,
     following the format specified in the project.
-    Ensures: list whose first element is the first expert and its resume up until the last expert
+    Ensures: list whose first element is the first expert or client and its resume up until the last expert
     """
 
     fileIn = open(fileName, 'r')
@@ -41,7 +45,7 @@ def readHeader(fileName):
     company as variables.
     Requires : fileName is str, the name of a .txt file listing experts,
     following the format specified in the project.
-    Ensures: day, time and company as str. """
+    Ensures: company as str and day and time as datetime format. """
 
     fileIn = open(fileName, 'r')
 
@@ -54,6 +58,7 @@ def readHeader(fileName):
     fileIn.readline()
     
     return (datetime.date(int(day[0:4]),int(day[5:7]),int(day[8:10])), datetime.time(int(time[0:2]),int(time[3:5])), company)
+
 
 
 
