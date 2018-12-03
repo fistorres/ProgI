9537a48d58158dd788e2d5b5c5c8e6225d3719a2
fistorres <45104156+fistorres@users.noreply.github.com>
2018-12-03 17:37:26 +0000

    Update filesReading.py

--- filesReading.py
+++ filesReading.py
@@ -3,12 +3,6 @@
 # 49187 Sofia Torres
 # 49269 Mário Gil Oliveira
 
-# 2018-2019 Programação 1 (LTI)
-# Grupo 38
-# 49187 Sofia Torres
-# 49269 Mário Gil Oliveira
-
-
 import datetime
 
 def readFile(fileName):
@@ -37,15 +31,13 @@ def readFile(fileName):
 
     return outputList
 
-
-
 def readHeader(fileName):
     """
     Converts a given file listing experts or clients and returns day, time,
     company as variables.
     Requires : fileName is str, the name of a .txt file listing experts,
     following the format specified in the project.
-    Ensures: company as str and day and time as datetime format. """
+    Ensures: company,day and time as str"""
 
     fileIn = open(fileName, 'r')
 
@@ -55,11 +47,9 @@ def readHeader(fileName):
     time = fileIn.readline().replace("\n", "")
     fileIn.readline()
     company = fileIn.readline().replace("\n", "")
-    fileIn.readline()
+    scope = fileIn.readline().replace("\n", "").replace(":","")
     
-    return (datetime.date(int(day[0:4]),int(day[5:7]),int(day[8:10])), datetime.time(int(time[0:2]),int(time[3:5])), company)
-
-
+    return (day,time,company,scope)
 
 
 
