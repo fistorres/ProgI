d82cda36a8e38b7df86f8e55906ba369a64aa2df
Gil Oliveira <mariogil@edu.ulisboa.pt>
2018-12-13 20:13:53 +0000

    fixed formated

--- formated.py
+++ formated.py
@@ -7,17 +7,18 @@
 
 from constants import hourMinutes
 
+
 def takeSpaces(listALL):
     """
     Edits a list of clients or experts and takes all the unnecessary blank spaces
     Requires: listALL as list 
-    Ensures: listALL with no unnecessary spaces
+    Ensures: listALL as list with no unnecessary spaces
     """
     # range starts at 1 because we don't want to eliminate
     # the spaces between the person's name
     for i in listALL:
-        for j in range(1,len(i)):
-            i[j] = str(i[j]).replace(" ","")
+        for j in range(1, len(i)):
+            i[j] = str(i[j]).replace(" ", "")
             
     return listALL
 
@@ -25,20 +26,21 @@ def takeSpaces(listALL):
 def formatExperts(listexp): # final list of experts to iterate on
     """
     Edits a list of lists of experts. Changes elements from str to another type better suited.
-    Requires: fileName is str, the name of a .txt file listing experts,
-    Ensures: ranking and pay as int and earnings as float and the experts areas as a tuple
+    Requires: listexp as a list of experts,
+    Ensures: a list with: skills as tuple, star rating and hourly pay as int, earnings as float
     """
 
     listexp = takeSpaces(listexp)
     for i in listexp:
-        i[2] = tuple(i[2].replace(";",",").replace("(","").replace(")","").split(","))
-        i[3] = int(i[3])
-        i[4] = int(i[4])
-        i[7] = float(i[7])
+        i[2] = tuple(i[2].replace(";", ",").replace("(", "").replace(")", "").split(","))  # skills as tuple
+        i[3] = int(i[3])  # star rating as int
+        i[4] = int(i[4])  # hourly pay as int
+        i[7] = float(i[7])  # earnings as float
         
     return listexp
 
-def formatClients(listcl): # final list of clients to itenerate on
+
+def formatClients(listcl):  # final list of clients to itenerate on
     """
     Edits a list of lists of clients. Changes elements from str to another type better suited.
     Requires: fileName is str, the name of a .txt file listing experts,
